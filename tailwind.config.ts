820b0d22b29439245cee09e33f8c559686b28809
Hoàng Nhân <102515276+Aikadev27@users.noreply.github.com>
2024-12-30 11:23:30 +0700

    setup layout, font, local colors, modify H tags

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,18 @@ export default {
   ],
   theme: {
     extend: {
+
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'dark_accent': '#EF6D58',
+        'dark_gray': '#391400',
+        'dark': '#391400',
+        'light': '#FFFFFF',
+        'light_accent': '#EF6D58',
+        'light_gray': '#FFFFFF',
+        'primary_btn_bg': 'var(--primary-btn-bg)',
+        'secondary_btn_bg': 'var(--secondary-btn-bg)',
+        'primary_btn_text': 'var(--primary-btn-text)',
+        'secondary_btn_text': 'var(--secondary-btn-text)',
       },
     },
   },
